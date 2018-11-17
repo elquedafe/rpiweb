633b1634a2c5ab6cd261d623083edb97dd369762
Daniel Garcia Anes <usrubuntu13@gmail.com>
2018-11-17 17:53:24 +0100

    ahora se puede escribir cuando se pasa al modo manual en el robot

--- fileHandler.py
+++ fileHandler.py
@@ -9,6 +9,7 @@ class FILEHANDLER:
 		return self._config['Parameters'][param]
 
 	def writeParam(self, param, value):
+		self._config.read("control.ini")
 		self._config.set('Parameters', param, value)
 		with open('control.ini', 'w') as configfile:
 			self._config.write(configfile)
