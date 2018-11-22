c005a0f11c9fb6bcb9149b9958efd57bcfc9e710
Daniel Garcia Anes <usrubuntu13@gmail.com>
2018-11-22 14:23:04 +0100

    boolean de bbdd a true

--- webSensor.py
+++ webSensor.py
@@ -349,6 +349,6 @@ def handleReader(lectura):
 #END REAL-TIME READING
 
 p = None #Proxy as global variable
-accesGranted = False
+accesGranted = True
 if __name__ == '__main__':
 	socketio.run(app, host='0.0.0.0', debug=True)
