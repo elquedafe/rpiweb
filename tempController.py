55f802fc1aba4f59769bbd8aff881254a9e6076f
Daniel Garcia Anes <usrubuntu13@gmail.com>
2018-11-22 22:36:09 +0100

    creado el hilo que lee cada segundo la temperatura, también se controla el fin ordando

--- tempController.py
+++ tempController.py
@@ -13,11 +13,11 @@ import telepot
 from telepot.loop import MessageLoop
 
 
-def serverThread(cond):
+def serverThread(cond, kill):
 	socket_s = socket.socket()
 	socket_s.bind(('127.0.0.1', 65000))
 	socket_s.listen(1)
-	while (1):
+	while (not kill.is_set()):
 		conn, addr = socket_s.accept()
 		global collected
 
@@ -28,9 +28,10 @@ def serverThread(cond):
 					cond.acquire()
 					collected = data.decode()
 					print(collected)
+					print('el server manda despertar')
 					cond.notify()
 					cond.release()
-					break;
+					break
 		except Exception as e:
 			print(e)
 		finally:
@@ -44,19 +45,26 @@ def robotStateChange(persona=None):
 	global bot
 	global telGroup
 	global temp
+	global kill
+
+	global mode
+
+	print('Temperatura: '+str(temp)+' -- '+'modo: '+mode+' --- ')
 
 	wEvent = open("eventos.txt", "a") #var to write events
+	fileH = fileHandler.FILEHANDLER()
 
 	if (collected == 'x00x01' or collected == 'x00x02'):
-		fileH = fileHandler.FILEHANDLER()
 		fileH.writeParam('Modo', 'manual')
 		wEvent.write(format(datetime.datetime.now())+"\t"+str(temp)+"\tcambio a modo manual\n")
 
 	elif (collected == 'x00x04'):
-		fileH = fileHandler.FILEHANDLER()
 		fileH.writeParam('Modo', 'automatico')
 		noti.sendNotification('Calefaccion automatica activada --- temperatura actual: '+str(temp)+' Celsius '+str(datetime.datetime.now()), bot, telGroup, persona)
 		wEvent.write(format(datetime.datetime.now())+"\t"+str(temp)+"\tcambio a modo automatico\n")
+	
+	elif (collected == 'x00x05'):
+		kill.set()
 
 	if collected == 'x00x01':
 		led.on()
@@ -75,12 +83,10 @@ def robotStateChange(persona=None):
 	#reset collected data
 	collected = None
 
-def robotThread(cond):
+def robotThread(cond, kill):
 	#global variables needed for the robot to run
 	global bot
 	global temp
-
-	sensor = proxy.PROXY() #creating sensor
 	global led
 	global noti
 
@@ -91,14 +97,13 @@ def robotThread(cond):
 		
 		notification = False #number of notifications is limit to 1 per on/off
 		cond.acquire()
-		while collected == None:
+		while collected == None and (not kill.is_set()):
 			cond.notify()
 			cond.release()
-			temp = sensor.leerTem()
 			wEvent = open("eventos.txt", "a") #var to write events
 			if (tempMax > temp):
 				if (notification == False):
-					noti.sendNotification('Manual: se enciende la calefaccion --- temperatura actual: '+str(temp)+' Celsius '+str(datetime.datetime.now()), bot, telGroup, persona)
+					noti.sendNotification('Auto: se enciende la calefaccion --- temperatura actual: '+str(temp)+' Celsius '+str(datetime.datetime.now()), bot, telGroup)
 					notification = True
 					led.on()
 					wEvent.write(format(datetime.datetime.now())+"\t"+str(temp)+"\tencendido\n")
@@ -107,10 +112,10 @@ def robotThread(cond):
 				if notification:
 					led.off()
 					wEvent.write(format(datetime.datetime.now())+"\t"+str(temp)+"\tapagado\n")
-					noti.sendNotification('Manual: se apaga la calefaccion --- temperatura actual: '+str(temp)+' Celsius '+str(datetime.datetime.now()), bot, telGroup, persona)
+					noti.sendNotification('Auto: se apaga la calefaccion --- temperatura actual: '+str(temp)+' Celsius '+str(datetime.datetime.now()), bot, telGroup)
 					notification = False
 			wEvent.close()
-			time.sleep(lec);
+			time.sleep(lec)
 			cond.acquire()
 
 		cond.notify()
@@ -119,14 +124,13 @@ def robotThread(cond):
 		#manual mode
 		#wait for user interaction
 		cond.acquire()
-		while collected == None:
+		while collected == None and (not kill.is_set()):
+			print('voy a dormir')
 			cond.wait()
+		print('estoy despierto')
 		cond.release()
 
-	temp = sensor.leerTem()
 	robotStateChange()
-	#closing sensor at the end
-	sensor.close()
 
 
 # Logica de comandos de Telegram
@@ -171,8 +175,17 @@ def lecturaMensajesBot(msg):
 		else:
 			bot.sendMessage(telGroup, "Comando: "+texto+" no valido. Accionado por "+persona)
 
-#params read from control.ini
+def readTemp(kill):
+	global temp
+	sensor = proxy.PROXY() #creating sensor
+	while not kill.is_set():
+		temp = sensor.leerTem()
+		time.sleep(1)
+	sensor.close()
+
+
 temp = None
+#params read from control.ini needed in children threads
 tempMin = None
 tempMax = None
 hume = None
@@ -187,62 +200,77 @@ x00x01 --> turn on led
 x00x02 --> turn off led
 x00x03 --> read again temp parameters
 x00x04 --> change from manual to auto with telegram
+x00x05 --> kill the program
 '''
 collected = None
-led = LED(18)
-bot = None
-noti = None
+led = LED(18) #GPIO
+bot = None #telegram
+noti = None #all notifications
+kill = None #it can kill the threads
 
 def main (args):
 	#var to read from config file
 	fileH = fileHandler.FILEHANDLER()
+
+	#notification initialization
 	global noti
 	noti = notificationHandler.NOTIFICATIONHANDLER()
 
+	#program
 	try:
 		global collected
-		global cond
+		global cond #needed for thread sync
 
+		#initializing telegram thread
 		global telToken
 		telToken = fileH.readParam('telegramtoken')
 		global telGroup
 		telGroup = fileH.readParam('telegramidgrupo')
 		global bot
 		bot = telepot.Bot(telToken)
-		#activa lecturas de comandos por parte del bot
+		#starting telegram thread
 		MessageLoop(bot, lecturaMensajesBot).run_as_thread()
 		
+		kill = threading.Event()
+		#starting the other threads
 		cond = threading.Condition()
-		serv = threading.Thread(target=serverThread, args=(cond,))
+		serv = threading.Thread(target=serverThread, args=(cond, kill,)) #thread which communicates with web server
 		serv.start()
-		while 1:
+		
+		while not kill.is_set():
 			try:
-				#reading config parameters
+				#reading temp & hume variables
 				global tempMin
 				tempMin = float(fileH.readParam('tempmin'))
 				global tempMax
 				tempMax = float(fileH.readParam('tempmax'))
 				global hume
 				hume = float(fileH.readParam('hume'))
+				#reading notification variables
 				global email
 				email = fileH.readParam('email')
-				#telegram variables
 				telToken = fileH.readParam('telegramtoken')
 				telGroup = fileH.readParam('telegramidgrupo')
-
+				#reading time interval
 				global lec
 				lec = float(fileH.readParam('intervalolectura'))
+				#reading mode
 				global mode
 				mode = fileH.readParam('modo')
 				if (mode == 'automatico'):
 					collected = None
-				
-				robot = threading.Thread(target=robotThread, args=(cond,))
+
+				#thread that reads temp value periodically
+				tempThread = threading.Thread(target=readTemp, args=(kill,))
+				tempThread.start()
+				time.sleep(1) #needed to wait for the first temp lecture
+				#starting robot
+				robot = threading.Thread(target=robotThread, args=(cond, kill))
 				robot.start()
 				robot.join()
 			except telepot.exception.TelegramError as e:
 				print(e)
-
+		tempThread.join()
 		serv.join()
 	except Exception as e:
 		print (e)
